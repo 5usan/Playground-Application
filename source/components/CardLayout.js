506e4bd741d3a8e67a1524913756d2275778a9fa
5usan <susanshrestha2056@gmail.com>
2022-06-14 16:09:01 +0545

    Animation added

--- source/components/CardLayout.js
+++ source/components/CardLayout.js
@@ -1,23 +1,48 @@
 import React from 'react';
-import {Image, Pressable, StyleSheet, Text, View} from 'react-native';
+import {Animated, Image, Pressable, StyleSheet, Text, View} from 'react-native';
 
 const CardLayout = ({title, backgroundColor, height, width, icon, onPress}) => {
+  const animation = new Animated.Value(0);
+  const inputRange = [0, 1];
+  const outputRange = [1, 0.93];
+  const scale = animation.interpolate({inputRange, outputRange});
+
+  const onPressIn = () => {
+    Animated.spring(animation, {
+      toValue: 1,
+      useNativeDriver: true,
+    }).start();
+  };
+  const onPressOut = () => {
+    Animated.spring(animation, {
+      toValue: 0,
+      useNativeDriver: true,
+    }).start();
+  };
   return (
-    <Pressable
-      onPress={onPress}
-      style={{
-        backgroundColor: backgroundColor,
-        height: height,
-        width: width,
-        marginBottom: 12,
-        borderRadius: 10,
-        padding: 10,
-      }}>
-      <Text style={styles.title}>{title.toUpperCase()}</Text>
-      <View style={styles.view}>
-        <Image source={icon} style={styles.icon} />
-      </View>
-    </Pressable>
+    <Animated.View
+      style={[
+        {
+          backgroundColor: backgroundColor,
+          height: height,
+          width: width,
+          marginBottom: 12,
+          borderRadius: 10,
+          padding: 10,
+        },
+        {transform: [{scale}]},
+      ]}>
+      <Pressable
+        onPress={onPress}
+        activeOpacity={1}
+        onPressIn={onPressIn}
+        onPressOut={onPressOut}>
+        <Text style={styles.title}>{title.toUpperCase()}</Text>
+        <View style={styles.view}>
+          <Image source={icon} style={styles.icon} />
+        </View>
+      </Pressable>
+    </Animated.View>
   );
 };
 
